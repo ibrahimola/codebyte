77252eea8219b4d1533ea5dac0549c54eda2e49c
ibrahimola <sheu_bello@yahoo.com>
2019-03-16 10:42:09 -0700

    reserve word challenge: done

--- FirstReverse.js
+++
@@ -1,8 +0,0 @@
-function FirstReverse(text){
-    text = "Hello coder and world";
-    
-    text.slipt(" ");
-    text.reverse();
-    text.join();
-}
-document.write(text);
---
+++ firstReserve.js
@@ -0,0 +1,8 @@
+
+// string and the is going reversed 
+// string, pass the method to it
+
+let value = "thenewbustom.com".reverse()
+
+
+console.log(value);
